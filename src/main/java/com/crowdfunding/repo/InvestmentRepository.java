856e0515baa087f487c7122efd934158e02322c6
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-06-21 17:19:25 +0530

    Update InvestmentRepository.java

--- src/main/java/com/crowdfunding/repo/InvestmentRepository.java
+++ src/main/java/com/crowdfunding/repo/InvestmentRepository.java
@@ -7,7 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.crowdfunding.entity.Investment;
 
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
-	List<Investment> findByProjectId(Long projectId);
 
-	List<Investment> findByInvestorName(String investorName);
+	// write your code for method to find list of investments associated with a specific project ID,
+	// and it must return data in list
+	
+
+	
+	// write your code for method to find list of investments made by an investor with the specified name,
+	// and it must return data in list
+	
 }
